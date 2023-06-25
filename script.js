235f4f411d9a28168232c2b7e6c83f893180505e
Sara-Poyo-Pastor <txara88@hotmail.com>
2023-06-26 01:41:12 +0200

    final

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const jokeBtn = document.getElementById('jokeBtn')
 jokeBtn.addEventListener('click', generateJoke)
 
 function generateJoke() {
- const config = {
+const config = {
     headers: {
     Accept: 'application/json',
 },
@@ -13,6 +13,6 @@ function generateJoke() {
 fetch('https://icanhazdadjoke.com', config)
     .then((res) => res.json())
     .then((data) => {
-     jokeEl.innerHTML = data.joke
+    jokeEl.innerHTML = data.joke
     })
 }
